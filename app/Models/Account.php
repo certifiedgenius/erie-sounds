7c2f4e1717b52bd94dd2b2e2b1ac3f041e30c962
certifiedgenius <mugeera.mansoor@medieinstitutet.se>
2022-02-08 10:48:49 +0100

    erie sounds new lines of code

---
+++ app/Models/Account.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model {
+	/**
+	 * Indicates if the model should be timestamped.
+	 *
+	 * @var bool
+	 */
+	public $timestamps = false;
+}
---
+++ config/app.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * App Config
+ */
+
+define('APP_NAME', 'Eloquent Bank');
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'bank');
+define('DB_USER', 'root');		// MAMP: `root`
+define('DB_PASSWORD', 'root');	// MAMP: `root`
