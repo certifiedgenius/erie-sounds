07e736af6ba3fddaf31697b27c9a0d5db410ec3f
certifiedgenius <mugeeramansoor@live.com>
2022-12-15 13:51:56 +0100

    SQL dump and Readme file instructions

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # erie-sounds
+
+- Change your host, name and database username and password in app.php 
+- Import the SQL file in the database folder into your database
--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@
 
 define('APP_NAME', 'Erie Sounds');
 
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'localhost:3306');
 define('DB_NAME', 'erie-sounds');
 define('DB_USER', 'root');		// MAMP: `root`
 define('DB_PASSWORD', 'root');	// MAMP: `root`
